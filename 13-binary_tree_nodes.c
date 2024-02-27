2fbcb2f06c0fabcd496d032d35b99ab4d7ef162c
mahakamal-e <mahakamal09@gmail.com>
2024-02-27 21:48:37 +0000

    Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,10 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left || tree->right)
-		count +=1;
+		count += 1;
+
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
 	return (count);
 }
