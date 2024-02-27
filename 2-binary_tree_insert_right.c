c78bebe4306d12576c1c32344e3a1b995539722e
mahakamal-e <mahakamal09@gmail.com>
2024-02-27 12:27:47 +0000

    task 3

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right)
 	{
 		n_node->right = parent->right;
-		parent->right->right = n_node;
+		parent->right->parent = n_node;
 	}
 	parent->right = n_node;
 	return (n_node);
