ff24f789cc1b8a987d3d2af4999fe45112e7738b
mahakamal-e <mahakamal09@gmail.com>
2024-02-29 15:31:31 -0800

    edit 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -37,8 +37,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
 
-	if (l_height > r_height)
-		return (l_height + 1);
-
-	return (r_height + 1);
+	return ((l_height > r_height) ? l_height + 1 : r_height + 1);
 }
